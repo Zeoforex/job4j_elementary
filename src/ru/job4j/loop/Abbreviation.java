362c1be120f9cfad23e0ed4bc364f07f7ba22312
Zeoforex <dshoma853@gmail.com>
2021-08-11 11:08:04 +0300

    Последнее задание по циклам

--- src/ru/job4j/loop/Abbreviation.java
+++ src/ru/job4j/loop/Abbreviation.java
@@ -2,6 +2,12 @@ package ru.job4j.loop;
 
 public class Abbreviation {
     public static String collect(String s) {
-        return s;
+        String[] words = s.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String word : words) {
+            char first = word.charAt(0);
+            stringBuilder.append(first);
+        }
+        return stringBuilder.toString();
     }
 }

7d678f2760796939432466e4a9600242d668595e
Zeoforex <dshoma853@gmail.com>
2021-08-12 21:50:27 +0300

    Плюс одна задача на ночь по массивам

---
+++ src/ru/job4j/array/ConcatArray.java
@@ -0,0 +1,8 @@
+package ru.job4j.array;
+
+public class ConcatArray {
+    public static int combineSize(int[] a, int[] b) {
+        int[] res = new int[a.length + b.length];
+        return res.length;
+    }
+}

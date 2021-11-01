430b8ce3ddf97539f5892b88aedc62a745561440
Zeoforex <dshoma853@gmail.com>
2021-11-01 17:42:36 +0300

    Исправил

--- src/main/java/ru/job4j/math/MathCalculator.java
+++ src/main/java/ru/job4j/math/MathCalculator.java
@@ -1,19 +1,17 @@
 package ru.job4j.math;
 
-import static ru.job4j.math.MathFunction.*;
-
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second) + multiply(first, second);
+        return MathFunction.sum(first, second) + MathFunction.multiply(first, second);
     }
 
     public static double differenceAndDivision(double first, double second) {
-        return difference(first, second) + division(first, second);
+        return MathFunction.difference(first, second) + MathFunction.division(first, second);
     }
 
     public static double sumAll(double first, double second) {
-        return sum(first, second) + difference(first, second) + multiply(first, second) + division(first, second);
+        return MathFunction.sum(first, second) + MathFunction.difference(first, second) + MathFunction.multiply(first, second) + MathFunction.division(first, second);
     }
 
     public static void main(String[] args) {
